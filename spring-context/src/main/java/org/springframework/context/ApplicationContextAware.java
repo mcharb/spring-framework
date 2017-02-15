64dc1db4696bc17e8a50adc88d6e04d9108a751a
mcharb <marc.charbonneau@gmail.com>
2017-02-15 17:25:00 -0500

    Update ApplicationContextAware.java
    Spurious change for demo

--- spring-context/src/main/java/org/springframework/context/ApplicationContextAware.java
+++ spring-context/src/main/java/org/springframework/context/ApplicationContextAware.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ 
+ MC: Modified the comment for the purpose of the demo
  */
 
 package org.springframework.context;
